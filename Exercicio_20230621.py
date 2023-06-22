6ccd79407ce56655836b97458690f655de53acf3
Letícia Lins <leticialins.rj@gmail.com>
2023-06-21 23:03:07 -0300

    Exercicios da aula de python.

---
+++ Exercicio_20230621.py
@@ -0,0 +1,87 @@
+#Exercício numero 1
+
+print("Exercício numero 1:")
+nome = "Letícia"
+idade = 38
+
+print("Olá, meu nome é", nome , "e eu tenho", idade , "anos")
+
+print()
+
+#Exercício numero 2
+print("Exercício numero 2:")
+frase = "Boa aula no curso de Python"
+
+print("Frase: ", frase)
+print("Comprimento da frase: ", len(frase))
+
+print()
+
+#Exercício numero 3
+print("Exercício numero 3:")
+nome = "Letícia"
+sobrenome = "Lins"
+
+nome_completo = nome + " " + sobrenome
+
+print("Nome completo: ", nome_completo)
+
+print()
+
+#Exercício numero 4
+print("Exercício numero 4:")
+
+frase = "Estou estudando no curso de Python"
+
+frasemaiuscula = frase.upper()
+print("Frase: ", frase)
+print("Frase em maiuscula: ", frasemaiuscula)
+
+print()
+
+#Exercício numero 5
+print("Exercício numero 5:")
+
+frase = "Aula de Python na quinta feira."
+palavras = frase.split()
+
+print("Frase: ", frase)
+print(palavras)
+
+print()
+
+#Exercício numero 6
+print("Exercício numero 6:")
+
+frase = "Aula de hoje vai ser Python"
+
+print("Frase antes: ", frase)
+print("Frase com palavra nova: ", frase.replace("Python" , "HTML e CSS"))
+
+print()
+
+#Exercício numero 7
+print("Exercício numero 7:")
+
+numero1 = 25
+numero2 = 50
+
+resultado_da_soma = numero1 + numero2
+
+print (numero1, "+", numero2)
+print("Resultado: ", resultado_da_soma)
+
+print()
+
+#Exercício numero 8
+print("Exercício numero 8:")
+num1 = 50
+num2 = 2
+
+resultado_da_multiplicação = num1 * num2
+print (num1, "X", num2)
+print("Resultado: ", resultado_da_multiplicação)
+
+
+
+
